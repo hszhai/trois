1461d1fce76a4b1c6ffd356ee287d746e6edab24
Sean Zhai <hszhai@syr.edu>
2022-10-08 12:09:03 -0400

    first build

--- src/Soleil.ts
+++ src/Soleil.ts
@@ -1,46 +1,50 @@
 import { makeNoise4D } from "open-simplex-noise"
 
+import { Scene, Vector3, Mesh, MeshBuilder, Curve3 } from '@babylonjs/core'
+
 export class Soleil {
     private _noise4D = makeNoise4D(1)
-    private _bb: BABYLON.Mesh
+    private _bb: Mesh
 
     constructor(
         options:{
-            pa: BABYLON.Vector3;
-            pb: BABYLON.Vector3;
+            pa: Vector3;
+            pb: Vector3;
             n?: number;
         }, 
-        scene: BABYLON.Scene
+        scene: Scene
     ) {
         const n_ = options.n || 5
-        const aa = BABYLON.MeshBuilder.CreateBox("", {}, scene)
+        const aa = MeshBuilder.CreateBox("", {}, scene)
         aa.position = options.pa
-        this._bb = BABYLON.MeshBuilder.CreateCylinder("", {updatable: true}, scene)
+        this._bb = MeshBuilder.CreateCylinder("", {updatable: true}, scene)
         this._bb.position = options.pb
 
         let pts = []
         for (let i=0; i<=n_; i++) {
             // // need to check ts: 1/5 = 0 ?
-            const ptmp = BABYLON.Vector3.Lerp(options.pa, options.pb, i/n_)
+            const ptmp = Vector3.Lerp(options.pa, options.pb, i/n_)
             const vtmp = this._noise4D(ptmp.x, ptmp.y, ptmp.z, 0)
             console.log(i, i/n_, vtmp)
             pts.push(
-                new BABYLON.Vector3(ptmp.x, ptmp.y + vtmp*10, ptmp.z)
+                new Vector3(ptmp.x, ptmp.y + vtmp*10, ptmp.z)
             )
         }
 
-        //const _c = new BABYLON.Path3D(pts)
-        const _c = BABYLON.Curve3.CreateCatmullRomSpline(pts, 20, false)
+        //const _c = new Path3D(pts)
+        const _c = Curve3.CreateCatmullRomSpline(pts, 20, false)
 
-        //const cur = BABYLON.Mesh.CreateLines("", _c.getPoints(), scene)
+        //const cur = Mesh.CreateLines("", _c.getPoints(), scene)
 
-        const cur = BABYLON.MeshBuilder.CreateLines("line", {points: _c.getPoints(),updatable: true}, scene)
-        //const c = BABYLON.Mesh.
+        //const cur = 
+        MeshBuilder.CreateLines("line", {points: _c.getPoints(),updatable: true}, scene)
+        //const c = Mesh.
 
 
     }
 
     update(dt:number) {
+        console.log(dt)
 
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import './style.css'
-import * as BABYLON from 'babylonjs' 
+import { Engine, Scene, ArcRotateCamera, HemisphericLight } from '@babylonjs/core'
+import { Vector3 } from '@babylonjs/core'
+
+
+//import * as BABYLON from 'babylonjs' 
 //import { makeNoise2D, makeNoise3D, makeNoise4D } from 'open-simplex-noise' 
 
 import { Soleil } from './Soleil'
@@ -8,28 +12,30 @@ const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-const engine = new BABYLON.Engine(canvas, true)
-const scene = new BABYLON.Scene(engine)
+const engine = new Engine(canvas, true)
+const scene = new Scene(engine)
 
-const camera = new BABYLON.ArcRotateCamera(
+const camera = new ArcRotateCamera(
   'camera', 
   -Math.PI / 2, Math.PI / 2.5, 15, 
-  new BABYLON.Vector3(0, 0, 0)
+  new Vector3(0, 0, 0)
 )
 camera.attachControl(canvas, true)
 
-const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+//const light = 
+new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
 
-const sol = new Soleil({pa:new BABYLON.Vector3(0,0,0), pb:new BABYLON.Vector3(12,10,12)}, scene)
+//const sol = 
+new Soleil({pa:new Vector3(0,0,0), pb:new Vector3(12,10,12)}, scene)
 
-
+/*
 const env = scene.createDefaultEnvironment();
 
 const xr = await scene.createDefaultXRExperienceAsync({
-    floorMeshes: [env.ground]
 });
 
 console.log(xr)
+*/
 
 engine.runRenderLoop(()=>{
   scene.render()
